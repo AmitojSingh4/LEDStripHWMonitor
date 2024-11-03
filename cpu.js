c46106cbe8e9c31fd4dcb1f9d4e29e88be2064b6
AmitojSingh4 <amitojsingh4153@gmail.com>
2024-11-03 11:55:58 +0000

    Reads data from win reg successfully
    used https://www.npmjs.com/package/regedit as previous solution did not work

--- cpu.js
+++ cpu.js
@@ -1,13 +1,10 @@
-const si = require('systeminformation');
-si.cpuTemperature().then(data => console.log(data));
+const regedit = require('regedit').promisified
 
-//Returned Values
-// {
-//     main: null,
-//     cores: [],
-//     max: null,
-//     socket: [],
-//     chipset: null,
-// }
-//https://systeminformation.io/cpu.html
-//Known issue
+async function main() {
+    const listResult = await regedit.list('HKCU\\SOFTWARE\\HWiNFO64\\VSB')
+    console.log(listResult)
+}
+
+main()
+
+//reg query HKEY_CURRENT_USER\SOFTWARE\HWiNFO64\VSB
